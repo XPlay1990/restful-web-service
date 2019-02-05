d39647a4b7c742e3d7ff83da88c8c0466f62d722
Jan Adamczyk <xiph2015@gmail.com>
2019-02-05 23:39:40 +0100

    basic auth

---
+++ src/main/java/com/qad/restfulwebservice/basicAuth/AuthenticationBean.java
@@ -0,0 +1,12 @@
+package com.qad.restfulwebservice.basicAuth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class AuthenticationBean {
+    private final String msg;
+}
