2e544b19befa335932defa906049fccb5662f45c
Jan Adamczyk <xiph2015@gmail.com>
2019-02-10 22:26:58 +0100

    prepare for jwt

---
+++ src/main/java/com/qad/restfulwebservice/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.qad.restfulwebservice.jwt.resource;
+
+public class AuthenticationException extends RuntimeException {
+	public AuthenticationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
