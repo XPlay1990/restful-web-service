b842c60448225961fbe5b307f47a05849bd5f68b
Jan Adamczyk <xiph2015@gmail.com>
2019-02-21 00:41:47 +0100

    generated id fixx

--- src/main/java/com/qad/restfulwebservice/todos/Todo.java
+++ src/main/java/com/qad/restfulwebservice/todos/Todo.java
@@ -4,18 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "todo")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Todo {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     private String username;
