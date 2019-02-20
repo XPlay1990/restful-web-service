bca3c99ac0e54fd97c06b912993f98139d340aa8
Jan Adamczyk <xiph2015@gmail.com>
2019-02-21 00:30:56 +0100

    MySQL connection

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,5 @@
-INSERT into todo(id, username, description, target_date, is_done)
-values(10001, 'jan', 'Learn Spring & Angular', sysdate(), false);
+insert into todo(id, username, description, target_date, is_done) values(1, 'jan', 'Learn Spring & Angular', CURDATE(), false);
 
-INSERT into todo(id, username, description, target_date, is_done)
-values(10002, 'jan', 'Start business', sysdate(), false);
+insert into todo(id, username, description, target_date, is_done) values(2, 'jan', 'Start business', CURDATE(), false);
 
-INSERT into todo(id, username, description, target_date, is_done)
-values(10003, 'jan', 'Get Rich', sysdate(), false);
+insert into todo(id, username, description, target_date, is_done) values(3, 'jan', 'Get Rich', CURDATE(), false);
