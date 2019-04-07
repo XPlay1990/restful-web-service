e15da1d015861aac0dca4ec301ac5336bc18d276
Jan Adamczyk <xiph2015@gmail.com>
2019-04-07 17:04:35 +0200

    temporary downgrade of java version for jenkins @ aws

--- src/test/java/com/qad/restfulwebservice/jwt/resource/JwtAuthenticationRestControllerTest.java
+++ src/test/java/com/qad/restfulwebservice/jwt/resource/JwtAuthenticationRestControllerTest.java
@@ -17,9 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Map;
-
-import static java.util.Map.entry;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
