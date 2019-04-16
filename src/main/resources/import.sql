d0ca7a8e7002e37485bb2f678be86ce78e0fe876
Jan Adamczyk <xiph2015@gmail.com>
2019-04-16 23:59:35 +0200

    db init

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,8 @@
-insert into todo(id, username, description, target_date, is_done) values(1, 'XPlay', 'Learn Spring & Angular', CURDATE(), false);
+insert IGNORE into todo(id, username, description, target_date, is_done)
+values (1, 'XPlay', 'Learn Spring & Angular', CURDATE(), false);
 
-insert into todo(id, username, description, target_date, is_done) values(2, 'XPlay', 'Start business', CURDATE(), false);
+insert IGNORE into todo(id, username, description, target_date, is_done)
+values (2, 'XPlay', 'Start business', CURDATE(), false);
 
-insert into todo(id, username, description, target_date, is_done) values(3, 'XPlay', 'Get Rich', CURDATE(), false);
+insert IGNORE into todo(id, username, description, target_date, is_done)
+values (3, 'XPlay', 'Get Rich', CURDATE(), false);
